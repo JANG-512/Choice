94f11bc2f2409448ef5b774e22fc07d237b67645
123 <123>
2022-04-26 16:29:32 +0900

    Initial Commit

---
+++ Choice/ChoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChoiceApp.swift
+//  Choice
+//
+//  Created by SARAM on 2022/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ChoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
